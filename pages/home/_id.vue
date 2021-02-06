74442ede0911395d5318c37724f9b21ab50bfd96
Pedro <pdrojcc@gmail.com>
2021-02-06 18:47:43 -0500

    Nuxt Metada data class added

---
+++ pages/home/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Home</div>
+</template>
+<script>
+  import homes from '~/data/homes'
+
+  export default {
+    created () {
+      console.log(this.$route)
+    }
+  }
+</script>
